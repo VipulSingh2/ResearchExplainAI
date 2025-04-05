7bf2adad7db7c54d1315f3ba88419d6a140e4539
Vipul Singh <v.ipulsingh592005@gmail.com>
2025-04-05 23:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,13 @@ import os
 load_dotenv()
 auth_token = os.getenv("GOOGLE_API_KEY")
 
-if not auth_token:
-    st.error("API key is not present. Firslty see that")
-    st.stop
 
 model = ChatGoogleGenerativeAI(
       model = "gemini-1.5-pro",
     temperature=0,
     max_tokens=None,
     max_retries=3,
-    api_key=auth_token,
+    api_key=os.getenv(GOOGLE_API_KEY),
     repetition_penalty = 1.03)
 
 prompt = load_prompt("prompt.json",encoding='utf-8')  
